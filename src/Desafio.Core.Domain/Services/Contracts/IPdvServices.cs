d7cf04b1490389c32d69339653bc121c4e1ba3d8
Rodrigo Amaro <jbravo.br@gmail.com>
2021-02-12 15:53:29 -0300

    Criação dos primeiros contratos de serviços do Domínio

---
+++ src/Desafio.Core.Domain/Services/Contracts/IPdvServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.Core.Domain.Services.Contracts
+{
+    public interface IPdvServices
+    {
+        Task<object> PayBill(object payment);
+    }
+}
---
+++ src/Desafio.Core.Domain/Services/Contracts/IUserServices.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Desafio.Core.Domain.Services.Contracts
+{
+    public interface IUserServices
+    {
+        Task<object> CheckUserExists(string email, string password);
+    }
+}
