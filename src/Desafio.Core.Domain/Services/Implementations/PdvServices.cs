56a3eb029691deb4aba5b0c77208c4505412f904
Rodrigo Amaro <jbravo.br@gmail.com>
2021-02-12 15:57:00 -0300

    Implementação dos contratos da camada de serviço do Domínio

---
+++ src/Desafio.Core.Domain/Services/Implementations/PdvServices.cs
@@ -0,0 +1,23 @@
+﻿using Desafio.Core.Domain.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio.Core.Domain.Services.Implementations
+{
+    public class PdvServices : IPdvServices
+    {
+        private readonly object _pdvRepository;
+
+        public PdvServices(object pdvRepository)
+        {
+            _pdvRepository = pdvRepository;
+        }
+
+        public async Task<object> PayBill(object payment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
