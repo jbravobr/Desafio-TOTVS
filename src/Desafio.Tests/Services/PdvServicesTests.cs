d4bc8a41a8ab222ee501a602290907fab6d76fe1
Rodrigo Amaro <jbravo.br@gmail.com>
2021-02-13 18:15:38 -0300

    Finalização dos Testes Unitários

---
+++ src/Desafio.Tests/Services/PdvServicesTests.cs
@@ -0,0 +1,68 @@
+﻿using Desafio.Core.Domain.Models;
+using Desafio.Core.Domain.Models.Enums;
+using Desafio.Core.Services.Implementations;
+using Desafio.Infra.Repositories.Contracts.Base;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Desafio.Tests.Services
+{
+    public class PdvServicesTests
+    {
+        private ICollection<PdvHistory> GetTestPdvHistories => new List<PdvHistory>
+        {
+            new PdvHistory
+            {
+                 Amount = 100,
+                  CreatedAt = DateTime.Now.AddDays(1),
+                   OperationType = OperationType.In
+            },
+            new PdvHistory
+            {
+                 Amount = 300,
+                  CreatedAt = DateTime.Now.AddDays(10),
+                   OperationType = OperationType.Out
+            }
+        };
+
+        private readonly Mock<IUnitOfWork> mockUnitOfWork;
+
+        public PdvServicesTests()
+        {
+            mockUnitOfWork = new Mock<IUnitOfWork>();
+        }
+
+        [Fact]
+        public void PayBill()
+        {
+            var amout = 100;
+            var total = 90;
+
+            mockUnitOfWork.Setup(x => x.PdvHistoryRepository.Insert(It.IsAny<PdvHistory>()));
+
+            var pdvServices = new PdvServices(mockUnitOfWork.Object);
+            var pdvResult = pdvServices.PayBill(amout, total);
+
+            mockUnitOfWork.Verify(x => x.PdvHistoryRepository.Insert(It.IsAny<PdvHistory>()), Times.Exactly(2));
+            Assert.NotNull(pdvResult);
+            Assert.True(pdvResult.IsClosed);
+            Assert.Equal(1, pdvResult.BankNotesToReturn[3]);
+        }
+
+        [Fact]
+        public void CheckUserExists()
+        {
+            mockUnitOfWork.Setup(x => x.PdvHistoryRepository.GetPaymentsHistory(It.IsAny<DateTime>(), It.IsAny<DateTime>())).Returns(GetTestPdvHistories);
+
+            var pdvServices = new PdvServices(mockUnitOfWork.Object);
+            var histories = pdvServices.GetPdvHistories(It.IsAny<DateTime>(), It.IsAny<DateTime>());
+
+            Assert.NotNull(histories);
+            Assert.Collection(histories,
+                              item => Assert.Equal(100, item.Amount),
+                              item => Assert.Equal(300, item.Amount));
+        }
+    }
+}
---
+++ src/Desafio.Tests/Services/UserServicesTests.cs
@@ -0,0 +1,34 @@
+﻿using Desafio.Core.Domain.Models;
+using Desafio.Core.Services.Implementations;
+using Desafio.Infra.Repositories.Contracts.Base;
+using Moq;
+using Xunit;
+
+namespace Desafio.Tests.Services
+{
+    public class UserServicesTests
+    {
+        private User GetTestUser => new User() { Email = "jbravo.br@gmail.com", Password = "P@ssw0rd!" };
+
+        private readonly Mock<IUnitOfWork> mockUnitOfWork;
+
+        public UserServicesTests()
+        {
+            mockUnitOfWork = new Mock<IUnitOfWork>();
+        }
+
+        [Fact]
+        public void CheckUserExists()
+        {
+            var testUser = GetTestUser;
+            mockUnitOfWork.Setup(x => x.UserRepository.CheckUserExists(testUser.Email, testUser.Password)).Returns(testUser);
+
+            var userServices = new UserServices(mockUnitOfWork.Object);
+            var user = userServices.CheckUserExists(testUser.Email, testUser.Password);
+
+            Assert.NotNull(user);
+            Assert.Equal("jbravo.br@gmail.com", user.Email);
+            Assert.Equal("P@ssw0rd!", user.Password);
+        }
+    }
+}
