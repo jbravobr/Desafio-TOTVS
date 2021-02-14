7dfd80e00be8d6fb0b3b3f3db47a73711059c3d9
Rodrigo Amaro <jbravo.br@gmail.com>
2021-02-13 22:20:42 -0300

    Correção na leitura do STREAM dentro do Middleware

--- src/Web/Middlewares/RequestResponseLoggingMiddleware.cs
+++ src/Web/Middlewares/RequestResponseLoggingMiddleware.cs
@@ -35,21 +35,18 @@ namespace Web.Middlewares
 
             await using var requestStream = _recyclableMemoryStreamManager.GetStream();
             await context.Request.Body.CopyToAsync(requestStream);
-            _logger.LogInformation($"Http Request Information:{Environment.NewLine}" +
+
+            var info = $"Http Request Information:{Environment.NewLine}" +
                                    $"Schema:{context.Request.Scheme} " +
                                    $"Host: {context.Request.Host} " +
                                    $"Path: {context.Request.Path} " +
                                    $"QueryString: {context.Request.QueryString} " +
-                                   $"Request Body: {ReadStreamInChunks(requestStream)}");
+                                   $"Request Body: {ReadStreamInChunks(requestStream)}";
+            _logger.LogInformation(info);
 
             var log = new Log()
             {
-                Request = $"Http Request Information:{Environment.NewLine}" +
-                                   $"Schema:{context.Request.Scheme} " +
-                                   $"Host: {context.Request.Host} " +
-                                   $"Path: {context.Request.Path} " +
-                                   $"QueryString: {context.Request.QueryString} " +
-                                   $"Request Body: {ReadStreamInChunks(requestStream)}",
+                Request = info,
                 CreateAt = DateTime.Now,
                 Id = Guid.NewGuid()
             };
@@ -69,21 +66,18 @@ namespace Web.Middlewares
 
             var text = await new StreamReader(context.Response.Body).ReadToEndAsync();
             context.Response.Body.Seek(0, SeekOrigin.Begin);
-            _logger.LogInformation($"Http Response Information:{Environment.NewLine}" +
+
+            var info = $"Http Response Information:{Environment.NewLine}" +
                                    $"Schema:{context.Request.Scheme} " +
                                    $"Host: {context.Request.Host} " +
                                    $"Path: {context.Request.Path} " +
                                    $"QueryString: {context.Request.QueryString} " +
-                                   $"Response Body: {text}");
+                                   $"Response Body: {text}";
+            _logger.LogInformation(info);
 
             var log = new Log()
             {
-                Response = $"Http Response Information:{Environment.NewLine}" +
-                                   $"Schema:{context.Request.Scheme} " +
-                                   $"Host: {context.Request.Host} " +
-                                   $"Path: {context.Request.Path} " +
-                                   $"QueryString: {context.Request.QueryString} " +
-                                   $"Response Body: {text}",
+                Response = info,
                 CreateAt = DateTime.Now,
                 Id = Guid.NewGuid()
             };
